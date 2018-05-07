4371a58f36fbcc5806e7faf55100220caaf22805
Unknown <lrbrowning6@gmail.com>
2018-05-07 12:32:18 -0400

    Built Class Intergration
    Will fill out classes in Google Calendar from given CSV

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import httplib2
 import os
+import csv
 
 from apiclient import discovery
 from oauth2client import client
@@ -21,7 +22,7 @@ SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'AutoSchedule'
 calendar_name = 'AutoSchedule'
-
+csv_name = 'schedule.csv'
 
 def get_credentials():
 	"""Gets valid user credentials from storage.
@@ -51,16 +52,47 @@ def get_credentials():
 		print('Storing credentials to ' + credential_path)
 	return credentials
 
+def get_csv():
+	rows = {}
+	# Open csv, extract all rows to rows object
+	with open(csv_name) as csvfile:
+		reader = csv.DictReader(csvfile)
+		csvLength = 1
+		for row in reader:
+			rows[str(csvLength)] = dict(row)
+			csvLength += 1
+	csvfile.close()
+	return rows, csvLength
+
+def create_events(day, formatted_date, rows, csvLength, service, calendar_id):
+	for x in range(1,csvLength):
+		a = str(x)
+		if rows[a]["Day"] == str(day):
+			sDateTime = formatted_date + "T" + rows[a]["Start Time"] + "-04:00"
+			eDateTime = formatted_date + "T" + rows[a]["End Time"] + "-04:00"
+			
+			event = {
+						'summary': rows[a]["Class"],
+						'start': {
+							'dateTime': sDateTime
+						},
+						'end': {
+							'dateTime': eDateTime
+						}
+					}
+			c_event = service.events().insert(calendarId=calendar_id, body=event).execute()
+			print('Event created. Class: {} Day: {}'.format(rows[a]["Class"], day))
+
 def main():
 	credentials = get_credentials()
 	http = credentials.authorize(httplib2.Http())
 	service = discovery.build('calendar', 'v3', http=http)
 
 	# Calendar List
-
 	calendar_id = ''
 	run = True
 
+	# Get all calendars, find specific one needed for this program
 	calendar_list = service.calendarList().list().execute()
 	for calendar_list_entry in calendar_list['items']:
 		if calendar_list_entry['summary'] == calendar_name:
@@ -73,51 +105,26 @@ def main():
 		}
 		created_calendar = service.calendars().insert(body=ecalendar).execute()
 		calendar_id = created_calendar['id']
-
 		print("Created Calendar! Name: {} ID: {}".format(calendar_name, calendar_id))
 	else: # Get Calendar
 		calendar = service.calendars().get(calendarId=calendar_id).execute()
 		print("Calendar Exists! Name: {} ID: {}".format(calendar_name, calendar_id))
 
-
-
-
+	# Check if program has already been run (events already created)
 	event_list = service.events().list(calendarId=calendar_id).execute()
 	for l_event in event_list['items']:
 		if 'Day' in l_event['summary']:
 			run = False
 
+	# Add events to calendar
 	if run:
 		d = datetime.datetime.now().date()
 		day = int(input("Today's Day: "))
-
-		for i in range(0,31):
+		rows, csvLength = get_csv()
+		for i in range(0,10):
 			if d.isoweekday() in range(1,6):
-				formatted_day = "Day {}".format(day)
 				formatted_date = str(d)
-
-				event = {
-					'summary': formatted_day,
-					'start': {
-						'date': formatted_date
-					},
-					'end': {
-						'date': formatted_date
-					},
-					'reminders': {
-						"useDefault": False,
-						'overrides': [
-							{
-								'method': 'popup',
-								'minutes': 420
-							}
-						]
-					}
-				}
-				
-				c_event = service.events().insert(calendarId=calendar_id, body=event).execute()
-				print('Event created. Date: {} Day: {}'.format(d, day))
-
+				create_events(day, formatted_date, rows, csvLength, service, calendar_id)
 				day += 1
 				
 				# Roll-Over
